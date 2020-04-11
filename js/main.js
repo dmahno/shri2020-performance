c15cb93cf016d4c264a547251e91682249ade127
dmahno <dmitrymahno@mail.ru>
2020-04-11 20:21:46 +0300

    reverted back all settings

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-'use strict';document.addEventListener('DOMContentLoaded',function(){const buttonsContainer=document.querySelector('.buttons-wrap');const fridgeInfoContainer=document.querySelector('.card_size_m:nth-child(8) .card-description');const purchaseListContainer=document.querySelector('.purchase-list-wrap');const confirmPurchaseButton=document.querySelector('.buttons-wrap .button_yellow');confirmPurchaseButton.onclick=()=>{fridgeInfoContainer.remove();buttonsContainer.style.display='none';purchaseListContainer.style.display='block'};document.getElementsByClassName('header-menu__switcher')[0].addEventListener('click',function(){document.getElementsByClassName('header-menu')[0].classList.toggle('header-menu_active')})},!1)
+let btns = document.querySelector('#btns');
+(btns.querySelector('.button_yellow').onclick = () => {
+  (document.querySelector('#purchase').style.display = 'block'),
+    (document.querySelector('#desc').style.display = 'none'),
+    (btns.style.display = 'none');
+}),
+  (document.querySelector('.header-menu__switcher').onclick = () => {
+    document
+      .querySelector('.header-menu')
+      .classList.toggle('header-menu_active');
+  });
